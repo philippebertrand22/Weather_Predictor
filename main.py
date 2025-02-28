7c180c116959bdfb2b8a8ed1c09e33586bb112ed
philippebertrand22 <90271955+philippebertrand22@users.noreply.github.com>
2025-02-28 15:08:22 -0500

    first interation
    testing the data set and running basic sql queries

---
+++ main.py
@@ -0,0 +1,63 @@
+#%%
+import glob
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import lit
+
+spark = SparkSession.builder.appName("MergedData").getOrCreate()
+
+folder_data = "data/"
+csv_files = glob.glob(folder_data + "*.csv")
+
+#print(csv_files)
+
+dfs = [spark.read.option("header", "true").csv(f) for f in csv_files]
+
+# Get all unique columns
+all_columns = set()
+for df in dfs:
+    all_columns.update(df.columns)
+
+# Convert to list
+all_columns = list(all_columns)
+
+def align_schema(df, all_columns):
+    for col_name in all_columns:
+        if col_name not in df.columns:
+            df = df.withColumn(col_name, lit(None))  # Add missing columns with NULL
+    return df.select(all_columns)  # Ensure consistent column order
+
+# Apply schema alignment
+dfs = [align_schema(df, all_columns) for df in dfs]
+
+# Merge all DataFrames
+final_df = dfs[0]
+for df in dfs[1:]:
+    final_df = final_df.union(df)
+
+keep_columns = ["STATION_NAME",
+                "LOCAL_YEAR",
+                "LOCAL_MONTH",
+                "LOCAL_DAY",
+                "LOCAL_HOUR",
+                "TEMP",
+                "PRECIP_AMOUNT"]
+
+final_df = final_df.select(keep_columns)
+
+#final_df.show()
+
+final_df.createOrReplaceTempView("weather_data")
+
+query = """
+    SELECT MIN(LOCAL_YEAR) AS min_year
+    FROM weather_data
+    WHERE PRECIP_AMOUNT IS NOT NULL
+"""
+
+sql = spark.sql(query)
+#sql = spark.sql("SELECT COUNT(*) AS total_rows FROM weather_data")
+
+sql.show()
+
+# %%
